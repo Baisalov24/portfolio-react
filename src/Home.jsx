a56cb7d87cefc19f66ce2b4484b0f3d437990eae
Tima Baisalov <baisalov.t98@gmail.com>
2024-04-08 06:54:17 +0600

    home style added

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,26 +7,20 @@ return (
     <section ref={scrollTab} className='home'>
         <div className="content">
             <div className="name">
-                MY NAME IS <span>LUNDEV</span>
+                MY NAME IS <span>Temirlan</span>
             </div>
             <div className="des">
                 {/* 30 */}
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores officiis beatae repellendus rem ullam, ipsam nemo dolorem dolorum illo laborum. Ea sed dolor ab qui, doloremque accusantium esse blanditiis possimus!
             </div>
             
-            <a href="/democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
-                Download My CV
+            <a href="#" target="_blank" rel="noopener noreferrer" className='animation active '>
+                My Github account
             </a>
         </div>
         <div className="avatar">
             <div className="card">
-                <img src="/avatar.jpg" alt="" />
-                <div className="info">
-                    <div>Developer</div>
-                    <div>VietNamese</div>
-                    <div>03/12</div>
-                    <div>Male</div>
-                </div>
+                <img src="/avatar.jpg" alt="avatar" />
             </div>
         </div>
     </section>
