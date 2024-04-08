c0a49602da6f328a9baa68f1a5905c544d05ba0b
Tima Baisalov <baisalov.t98@gmail.com>
2024-04-08 06:56:27 +0600

    unnecessary styles removed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,37 @@
-import React, { useRef } from 'react' 
+import React, { useRef } from "react";
+
 function Home() {
-const scrollTab = useRef();
+  const scrollTab = useRef();
 
+  return (
+    <section ref={scrollTab} className="home">
+      <div className="content">
+        <div className="name">
+          MY NAME IS <span>Temirlan</span>
+        </div>
+        <div className="des">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
+          officiis beatae repellendus rem ullam, ipsam nemo dolorem dolorum illo
+          laborum. Ea sed dolor ab qui, doloremque accusantium esse blanditiis
+          possimus!
+        </div>
 
-return (
-    <section ref={scrollTab} className='home'>
-        <div className="content">
-            <div className="name">
-                MY NAME IS <span>Temirlan</span>
-            </div>
-            <div className="des">
-                {/* 30 */}
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores officiis beatae repellendus rem ullam, ipsam nemo dolorem dolorum illo laborum. Ea sed dolor ab qui, doloremque accusantium esse blanditiis possimus!
-            </div>
-            
-            <a href="#" target="_blank" rel="noopener noreferrer" className='animation active '>
-                My Github account
-            </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="animation active "
+        >
+          My Github account
+        </a>
+      </div>
+      <div className="avatar">
+        <div className="card">
+          <img src="/avatar.jpg" alt="avatar" />
         </div>
-        <div className="avatar">
-            <div className="card">
-                <img src="/avatar.jpg" alt="avatar" />
-            </div>
-        </div>
+      </div>
     </section>
-    )
+  );
 }
 
-export default Home
+export default Home;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,33 +2,31 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-const NavBar = ({activeTab}) => {
-    const [linkNav] = useState(['Home', 'Skills', 'Projects', 'Contacts']);
-    const [statusNav, changeStatusNav] = useState(null);
-    const toggleNav = () => {
-        changeStatusNav(statusNav === null ? 'active' : null);
-    }
+const NavBar = ({ activeTab }) => {
+  const [linkNav] = useState(["Home", "Skills", "Projects", "Contacts"]);
+  const [statusNav, changeStatusNav] = useState(null);
+  const toggleNav = () => {
+    changeStatusNav(statusNav === null ? "active" : null);
+  };
 
-    
-    return (
-        <header>
-            <div className="logo">
-                <img src="/logo.png" alt="logo"/>My Portfolio
-            </div>
-            <nav className={statusNav}>
-                {
-                linkNav.map(value => (
-                    <span key={value} 
-                    className={activeTab === value ? 'active': ''}
-                    >{value}</span>
-                ))
-                }
-            </nav>
-            <div className="icon-bar" onClick={toggleNav}>
-                <FontAwesomeIcon icon={faBars} />
-            </div>
-        </header>
-    )
-}
+  return (
+    <header>
+      <div className="logo">
+        <img src="/logo.png" alt="logo" />
+        My Portfolio
+      </div>
+      <nav className={statusNav}>
+        {linkNav.map((value) => (
+          <span key={value} className={activeTab === value ? "active" : ""}>
+            {value}
+          </span>
+        ))}
+      </nav>
+      <div className="icon-bar" onClick={toggleNav}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+    </header>
+  );
+};
 
-  export default NavBar;
+export default NavBar;
