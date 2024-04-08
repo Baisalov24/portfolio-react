ef69b4ad29dbfb8eb8814ebdcd1e648a902004c2
Tima Baisalov <baisalov.t98@gmail.com>
2024-04-08 07:13:32 +0600

    projects component created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Skills from "./Skills";
+import Projects from "./Projects";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Home/>
     <Skills/>
+    <Projects />
     </div>
   );
 }
