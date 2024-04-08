dd5c8789a56e7e891cfcc65349d3b203b2bca3bc
Tima Baisalov <baisalov.t98@gmail.com>
2024-04-08 07:04:01 +0600

    skills part added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css'
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Skills from "./Skills";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+    <Skills/>
     </div>
   );
 }
