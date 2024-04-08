20a8885a73183788e9729772d7d01ae218bc399d
Tima Baisalov <baisalov.t98@gmail.com>
2024-04-08 13:06:03 +0600

    experience component created

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./components/Home/Home";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contacts from "./components/Contacts/Contacts";
+import Experience from "./components/Experience/Experience";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Experience />
       <Skills />
       <Projects />
       <Contacts />
---
+++ src/components/Experience/experienceData.js
@@ -0,0 +1,44 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Lorem ipsum",
+    location: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste assumenda suscipit delectus eum. Quia, dolores corporis quaerat ex exercitationem sed iure obcaecati aliquid dolorem fuga ",
+    buttonText: "Lorem ipsum",
+    date: "Lorem ipsum",
+    icon: "Lorem ipsum",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum",
+    location: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste assumenda suscipit delectus eum. Quia, dolores corporis quaerat ex exercitationem sed iure obcaecati aliquid dolorem fuga ",
+    buttonText: "Lorem ipsum",
+    date: "Lorem ipsum",
+    icon: "Lorem ipsum",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum",
+    location: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste assumenda suscipit delectus eum. Quia, dolores corporis quaerat ex exercitationem sed iure obcaecati aliquid dolorem fuga ",
+    buttonText: "Lorem ipsum",
+    date: "Lorem ipsum",
+    icon: "Lorem ipsum",
+  },
+  {
+    id: 4,
+    title: "Lorem ipsum",
+    location: "Lorem ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste assumenda suscipit delectus eum. Quia, dolores corporis quaerat ex exercitationem sed iure obcaecati aliquid dolorem fuga ",
+    buttonText: "Lorem ipsum",
+    date: "Lorem ipsum",
+    icon: "Lorem ipsum",
+  },
+];
+
+export default timelineElements;
